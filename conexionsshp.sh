5034543dabe1d1f7e40eed44a70b66f2275881cf
malejandragoco <malejandragoco@hotmail.com>
2017-02-13 10:46:17 -0500

    Actualizado con versión de borrado y apagado

--- conexionsshp.sh
+++ conexionsshp.sh
@@ -16,7 +16,6 @@ parallel --tag --nonall -S 4/pi1@192.168.2.111,4/pi2@192.168.2.112,4/pi3@192.168
 
 parallel --tag --nonall -S 4/pi1@192.168.2.111,4/pi2@192.168.2.112,4/pi3@192.168.2.113,4/pi4@192.168.2.114,4/pi5@192.168.2.115 "${scriptfoto}"  
 
-
 scp pi1@192.168.2.111:~/$1.jpg  /home/tg1628/Documentos/Rasp1/ 
 scp pi2@192.168.2.112:~/$1.jpg  /home/tg1628/Documentos/Rasp2/ 
 scp pi3@192.168.2.113:~/$1.jpg  /home/tg1628/Documentos/Rasp3/ 

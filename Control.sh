306be78b07cea93e8cc373e7dd6a6ab036974596
malejandragoco <malejandragoco@hotmail.com>
2017-02-13 10:47:32 -0500

    Actualizado con borrado y apagado

--- Control.sh
+++ Control.sh
@@ -3,12 +3,35 @@
 #El siguiente script es el encargado de llamar los demas procesos.
 
 NombreFoto=$1
+Counter=0;
+Raspberrys="192.168.2.100 192.168.2.111 192.168.2.112 192.168.2.113 192.168.2.114 192.168.2.115"
 
 #Primero se verifica si las tarjetas están conectadas a la red local junto con el pc.
-sh ./ActividadRpis.sh
+for Rpis in ${Raspberrys} ; do
+	ping -c1 ${Rpis} #-c1 manda un solo ping
+	if [ $? -eq 0 ] #$?Da el estatus de salida del comando anterior
+		then
+			
+			Counter=$((Counter+1))
+			
+	fi
+done
 
 #Luego se verifica el retraso del servidor NTP y se captura la foto.
-sh ./conexionsshp.sh $1
+
+if [ $Counter -eq 6 ] #$?Da el estatus de salida del comando anterior
+		then
+			echo "EL PC Y LAS RASPBERRYS ESTÁN LISTAS PARA TRABAJAR."
+			bash ./conexionsshp.sh $1
+			bash ./BorrarT.sh $1
+			echo "Se ha borrado la imagen de cada una de las raspberrys."
+			bash ./ApagadoT.sh
+			exit 0
+		else
+			echo "VERIFIQUE LA CONEXION."
+fi
+
+
 
 
 

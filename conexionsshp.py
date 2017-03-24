ade5722f3ba316fc632f40d75bbd177bb6247c16
malejandragoco <malejandragoco@hotmail.com>
2017-03-24 10:08:56 -0500

    Primera Version en Python

---
+++ conexionsshp.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+
+#O. Tange (2011): GNU Parallel - The Command-Line Power Tool,
+#;login: The USENIX Magazine, February 2011:42-47.
+
+import os
+
+os.system("parallel --tag --nonall -S 4/pi1@192.168.2.111,4/pi2@192.168.2.112,4/pi3@192.168.2.113,4/pi4@192.168.2.114,4/pi5@192.168.2.115 ntpq -p")

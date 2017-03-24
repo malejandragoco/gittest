0c8fa9f47c281d44f202c648c34f1476abf4b2e3
malejandragoco <malejandragoco@hotmail.com>
2017-03-24 10:10:56 -0500

    Primera Version en Python

--- DiaFoto.sh
+++ DiaFoto.sh
@@ -2,8 +2,8 @@
 
 NombreFoto=$1
 
-identify -verbose /home/tg1628/Documentos/Rasp1/$1.jpg | grep "exif:DateTimeOriginal:"
-identify -verbose /home/tg1628/Documentos/Rasp2/$1.jpg | grep "exif:DateTimeOriginal:"
-identify -verbose /home/tg1628/Documentos/Rasp3/$1.jpg | grep "exif:DateTimeOriginal:"
-identify -verbose /home/tg1628/Documentos/Rasp4/$1.jpg | grep "exif:DateTimeOriginal:"
-identify -verbose /home/tg1628/Documentos/Rasp5/$1.jpg | grep "exif:DateTimeOriginal:"
+identify -verbose /home/tg1628/Documentos/Rasp1/$12000.jp2 | grep "exif:DateTimeOriginal:"
+identify -verbose /home/tg1628/Documentos/Rasp2/$12000.jp2 | grep "exif:DateTimeOriginal:"
+identify -verbose /home/tg1628/Documentos/Rasp3/$12000.jp2 | grep "exif:DateTimeOriginal:"
+identify -verbose /home/tg1628/Documentos/Rasp4/$12000.jp2 | grep "exif:DateTimeOriginal:"
+identify -verbose /home/tg1628/Documentos/Rasp5/$12000.jp2 | grep "exif:DateTimeOriginal:"

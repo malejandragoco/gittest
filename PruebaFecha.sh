bdfd2c8369bd4e059f92559eb3ea51709c9478a2
malejandragoco <malejandragoco@hotmail.com>
2017-03-24 10:11:48 -0500

    Primera Version en Python

--- PruebaFecha.sh
+++ PruebaFecha.sh
@@ -3,9 +3,9 @@
 NombreFoto=$1
 
 while true; do
-todate=$(date +%H%M%S )
+todate=$(date +%H%M%S%N )
 #echo $todate
-if [ $todate -eq 180100 ]
+if [ $todate -ge 102100900000000 ]
 then
 	#echo "yes"
 	raspistill -w 800 -h 600 -ISO 800 -n -t 125 -q 40 -ex off -ss 5000 -a 4 -a "%s" -o $1.jpg

d97abcc3f09a5a62fda97561da418e94404c6e2f
malejandragoco <malejandragoco@hotmail.com>
2017-05-03 16:58:11 -0500

    Actualizado 3 de mayo

--- ControlMENB.sh
+++ ControlMENB.sh
@@ -10,20 +10,20 @@ then
 	echo "EL MAESTRO ES EL 1"
 	scp /home/tg1628/gittest/SerialMNB.c pi1@192.168.2.111:~/Documents/
 	scp /home/tg1628/gittest/SerialENB.c pi2@192.168.2.112:~/Documents/
-	#scp /home/tg1628/gittest/SerialENB.c pi3@192.168.2.113:~/Documents/
-	#scp /home/tg1628/gittest/SerialENB.c pi4@192.168.2.114:~/Documents/
-	#scp /home/tg1628/gittest/SerialENB.c pi5@192.168.2.115:~/Documents/
+	scp /home/tg1628/gittest/SerialENB.c pi3@192.168.2.113:~/Documents/
+	scp /home/tg1628/gittest/SerialENB.c pi4@192.168.2.114:~/Documents/
+	scp /home/tg1628/gittest/SerialENB.c pi5@192.168.2.115:~/Documents/
 	scp /home/tg1628/gittest/M/Makefile pi1@192.168.2.111:~/Documents/
 	scp /home/tg1628/gittest/E/Makefile pi2@192.168.2.112:~/Documents/
-	#scp /home/tg1628/gittest/E/Makefile pi3@192.168.2.113:~/Documents/
-	#scp /home/tg1628/gittest/E/Makefile pi4@192.168.2.114:~/Documents/
-	#scp /home/tg1628/gittest/E/Makefile pi5@192.168.2.115:~/Documents/
+	scp /home/tg1628/gittest/E/Makefile pi3@192.168.2.113:~/Documents/
+	scp /home/tg1628/gittest/E/Makefile pi4@192.168.2.114:~/Documents/
+	scp /home/tg1628/gittest/E/Makefile pi5@192.168.2.115:~/Documents/
 
-	parallel --tag --nonall -S 4/pi1@192.168.2.111,4/pi2@192.168.2.112 "${compilar}"
+	parallel --tag --nonall -S 4/pi1@192.168.2.111,4/pi2@192.168.2.112,4/pi3@192.168.2.113,4/pi4@192.168.2.114,4/pi5@192.168.2.115 "${compilar}"
+	
+	#parallel --tag --nonall -S 4/pi2@192.168.2.112 "${correresclavo}"
 
-	parallel --tag --nonall -S 4/pi2@192.168.2.112 "${correresclavo}"
-
-	ssh pi1@192.168.2.111 "${corrermaestro}"
+	#ssh pi1@192.168.2.111 "${corrermaestro}"
 
 	exit 0
 else
@@ -46,9 +46,9 @@ then
 
 	parallel --tag --nonall -S 4/pi1@192.168.2.111,4/pi2@192.168.2.112,4/pi3@192.168.2.113,4/pi4@192.168.2.114,4/pi5@192.168.2.115 "${compilar}"
 
-	parallel --tag --nonall -S 4/pi1@192.168.2.111,4/pi3@192.168.2.113,4/pi4@192.168.2.114,4/pi5@192.168.2.115 "${correresclavo}"
+	#parallel --tag --nonall -S 4/pi1@192.168.2.111,4/pi3@192.168.2.113,4/pi4@192.168.2.114,4/pi5@192.168.2.115 "${correresclavo}"
 
-	ssh pi2@192.168.2.112 "${corrermaestro}"
+	#ssh pi2@192.168.2.112 "${corrermaestro}"
 
 	exit 0
 else
@@ -71,9 +71,9 @@ then
 
 	parallel --tag --nonall -S 4/pi1@192.168.2.111,4/pi2@192.168.2.112,4/pi3@192.168.2.113,4/pi4@192.168.2.114,4/pi5@192.168.2.115 "${compilar}"
 
-	parallel --tag --nonall -S 4/pi2@192.168.2.112,4/pi1@192.168.2.111,4/pi4@192.168.2.114,4/pi5@192.168.2.115 "${correresclavo}"
+	#parallel --tag --nonall -S 4/pi2@192.168.2.112,4/pi1@192.168.2.111,4/pi4@192.168.2.114,4/pi5@192.168.2.115 "${correresclavo}"
 
-	ssh pi3@192.168.2.113 "${corrermaestro}"
+	#ssh pi3@192.168.2.113 "${corrermaestro}"
 
 	exit 0
 else
@@ -96,9 +96,9 @@ then
 
 	parallel --tag --nonall -S 4/pi1@192.168.2.111,4/pi2@192.168.2.112,4/pi3@192.168.2.113,4/pi4@192.168.2.114,4/pi5@192.168.2.115 "${compilar}"
 
-	parallel --tag --nonall -S 4/pi2@192.168.2.112,4/pi3@192.168.2.113,4/pi1@192.168.2.111,4/pi5@192.168.2.115 "${correresclavo}"
+	#parallel --tag --nonall -S 4/pi2@192.168.2.112,4/pi3@192.168.2.113,4/pi1@192.168.2.111,4/pi5@192.168.2.115 "${correresclavo}"
 
-	ssh pi4@192.168.2.114 "${corrermaestro}"
+	#ssh pi4@192.168.2.114 "${corrermaestro}"
 	
 	exit 0
 else
@@ -121,9 +121,9 @@ then
 	
 	parallel --tag --nonall -S 4/pi1@192.168.2.111,4/pi2@192.168.2.112,4/pi3@192.168.2.113,4/pi4@192.168.2.114,4/pi5@192.168.2.115 "${compilar}"
 
-	parallel --tag --nonall -S 4/pi2@192.168.2.112,4/pi3@192.168.2.113,4/pi4@192.168.2.114,4/pi1@192.168.2.111 "${correresclavo}"
+	#parallel --tag --nonall -S 4/pi2@192.168.2.112,4/pi3@192.168.2.113,4/pi4@192.168.2.114,4/pi1@192.168.2.111 "${correresclavo}"
 
-	ssh pi5@192.168.2.115 "${corrermaestro}"
+	#ssh pi5@192.168.2.115 "${corrermaestro}"
 	
 	exit 0
 else

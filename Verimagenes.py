ca098206d36449d6c681b724b80b9b2b0a823081
malejandragoco <malejandragoco@hotmail.com>
2017-05-03 17:04:04 -0500

    Actualizado 3 de mayo

--- Verimagenes.py
+++ Verimagenes.py
@@ -3,7 +3,7 @@
 import os
 import sys
 
-nombreimagen2000 = sys.argv[1]+"2000.jp2" 
+nombreimagen2000= sys.argv[1]+"2000.jp2" 
 
 os.system("convert ~/Documentos/Rasp1/"+nombreimagen2000+" ~/Documentos/Rasp2/"+nombreimagen2000+"  ~/Documentos/Rasp3/"+nombreimagen2000+"   ~/Documentos/Rasp4/"+nombreimagen2000+"   ~/Documentos/Rasp5/"+nombreimagen2000+"   +append ~/Documentos/mosaicos/"+nombreimagen2000+"")
 
